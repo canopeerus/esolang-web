0226c69e9cb94ab715dc53d975243483999d6a23
canopeerus <visvanathannaditya@gmail.com>
2021-10-06 22:59:26 -0600

    Basic Editor Components for console and editor
    * Add BlazorMonaco as dependency package to render editor windows
    * Move ConsoleComponent and EditorComponent to their own
      namespace/folders
    * Render basic Monaco editors for the editor and console, renders
      console as a read-only monaco instance.
    * Break each razor component into separate source files for code, markup
      and style - (.cs, .razor, .css)
    * Editor CSS file is not here....yet....

---
+++ esolang-webrunner/Components/ConsoleWindow/ConsoleComponent.razor.cs
@@ -0,0 +1,30 @@
+﻿using BlazorMonaco;
+
+namespace esolang_webrunner.Components.ConsoleWindow
+{
+    public partial class ConsoleComponent
+    {
+        private string outputText { get; set; }
+
+        public StandaloneEditorConstructionOptions getEditorConstructionOptions(MonacoEditor editor)
+        {
+            return new StandaloneEditorConstructionOptions
+            {
+                Language = "javascript",
+                Value = "yolo",
+                Dimension = new Dimension
+                {
+                    Height = 200
+                },
+                ReadOnly = true,
+                Theme = "hc-black",
+                Folding = false,
+                LineNumbers = "off",
+                AutomaticLayout = true,
+                RoundedSelection = true
+
+            };
+        }
+    }
+}
+
---
+++ esolang-webrunner/Components/EditorWindow/EditorComponent.razor.cs
@@ -0,0 +1,32 @@
+﻿using BlazorMonaco;
+
+namespace esolang_webrunner.Components.EditorWindow
+{
+    public partial class EditorComponent
+    { 
+        public string language { get; set; }
+
+        public StandaloneEditorConstructionOptions getEditorConstructionOptions(MonacoEditor editor)
+        {
+            return new StandaloneEditorConstructionOptions
+            {
+                Language = language,
+                Value = "yolobhaisaab",
+                Dimension = new Dimension
+                {
+                    Height = 500
+                },
+                Folding = false,
+                AutomaticLayout = true,
+                Theme = "vs-dark",
+                QuickSuggestions = new QuickSuggestionsOptions
+                {
+                    Comments = false,
+                    Other = false,
+                    Strings = false
+                }
+            };
+        }
+    }
+}
+
